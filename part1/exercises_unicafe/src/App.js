4fdce2e8f702dacf94ed2494b2df65f1eb306997
Andre Low <andre-low@users.noreply.github.com>
2021-08-16 17:38:00 +0800

    Completed exercise 1.6

--- part1/exercises_unicafe/src/App.js
+++ part1/exercises_unicafe/src/App.js
@@ -1,5 +1,33 @@
 import React, { useState } from 'react'
 
+const Button = ({label, handleClick}) =>
+  <button onClick = {handleClick}>{label}</button>
+
+const FeedbackCollector = ({state:[good, neutral, bad], stateHandlers:[setGood, setNeutral, setBad]}) => {
+  const giveGoodFeedback = () => setGood(good + 1)
+  const giveNeutralFeedback = () => setNeutral(neutral + 1)
+  const giveBadFeedback = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <p>How was your experience at Unicafe?</p>
+      <Button label = 'Good 😊' handleClick = {giveGoodFeedback} /> 
+      <Button label = 'Neutral 😐' handleClick = {giveNeutralFeedback} /> 
+      <Button label = 'Bad 😞' handleClick = {giveBadFeedback} />
+    </div>
+  )
+}
+
+const Statistics = ({state:[good, neutral, bad]}) => (
+  <div>
+    <h1>Statistics</h1>
+    Good: {good}<br />
+    Neutral: {neutral}<br />
+    Bad: {bad}<br />
+  </div>
+)
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -7,9 +35,10 @@ const App = () => {
   const [bad, setBad] = useState(0)
 
   return (
-    <div>
-      code here
-    </div>
+    <>
+      <FeedbackCollector state = {[good, neutral, bad]} stateHandlers = {[setGood, setNeutral, setBad]}/>
+      <Statistics state = {[good, neutral, bad]}/>
+    </>
   )
 }
 
