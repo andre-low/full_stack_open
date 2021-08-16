2607341c5b9b35f9fa99e93a849890f1f8bcb262
Andre Low <andre-low@users.noreply.github.com>
2021-08-16 18:50:49 +0800

    Completed ex 1.14

--- part1/exercises_anecdotes/src/App.js
+++ part1/exercises_anecdotes/src/App.js
@@ -4,6 +4,7 @@ const Button = ({label, handleClick}) =>
   <button onClick = {handleClick}>{label}</button>
 
 const App = () => {
+  // Store anecdotes
   const anecdotes = [
     'If it hurts, do it more often',
     'Adding manpower to a late software project makes it later!',
@@ -13,10 +14,12 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
   ]
-   
+  
+  // Set state
   const [selected, setSelected] = useState(0)
   const [votes, setVotes] = useState(new Array(7).fill(0))
 
+  // Click handler functions
   const randomSelected = () => setSelected(Math.floor(Math.random() * 7))
   const upvoteAnecdote = () => {
     const votesNew = [...votes]
