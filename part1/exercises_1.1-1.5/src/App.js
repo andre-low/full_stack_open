0010a2b57816b2cbe98453742296137588d2a8b7
Andre Low <andre-low@users.noreply.github.com>
2022-07-28 16:24:09 +0800

    Added props type checking

--- part1/exercises_1.1-1.5/src/App.js
+++ part1/exercises_1.1-1.5/src/App.js
@@ -1,22 +1,44 @@
-const Header = (props) => <h1>{props.course.name}</h1>
+import PropTypes from 'prop-types';
 
-const Part = (props) => (<p>{props.part.name} {props.part.exercises}</p>)
+const Header = (props) => <h1>{props.course.name}</h1>;
+
+Header.propTypes = {
+  course: PropTypes.object.isRequired,
+};
+
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+);
+
+Part.propTypes = {
+  part: PropTypes.object.isRequired,
+};
 
 const Content = (props) => (
   <>
-    <Part part = {props.course.parts[0]} />
-    <Part part = {props.course.parts[1]} />
-    <Part part = {props.course.parts[2]} />
+    <Part part={props.course.parts[0]} />
+    <Part part={props.course.parts[1]} />
+    <Part part={props.course.parts[2]} />
   </>
-)
+);
+
+Content.propTypes = {
+  course: PropTypes.object.isRequired,
+};
 
 const Total = (props) => {
-  let exercises = 0
+  let exercises = 0;
   for (const part of props.course.parts) {
-    exercises += part.exercises
+    exercises += part.exercises;
   }
-  return <p>Number of exercises {exercises}</p>
-}
+  return <p>Number of exercises {exercises}</p>;
+};
+
+Total.propTypes = {
+  course: PropTypes.object.isRequired,
+};
 
 const App = () => {
   const course = {
@@ -24,26 +46,26 @@ const App = () => {
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
       },
       {
         name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
-      <Header course = {course} />
-      <Content course = {course} />
-      <Total course = {course} />
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
