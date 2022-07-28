d09194b9ef5c4a9d9b7a9f1958d588387b789a56
Andre Low <andre-low@users.noreply.github.com>
2022-07-28 12:38:09 +0800

    Some refactoring, adding comments

--- part1/a_intro_to_react/src/App.js
+++ part1/a_intro_to_react/src/App.js
@@ -1,34 +1,32 @@
-import React from 'react'
-const Hello = (props) => {
-  return (
-    <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
-    </div>
-  )
-}
+// Using concise returns in the two following arrow functions, see: https://www.samanthaming.com/tidbits/47-arrow-functions-cheatsheet/
 
-const Footer = () => {
-  return (
-    <div>
-      greeting app created by <a href="https://github.com/mluukkai">mluukkai</a>
-    </div>
-  )
-}
+const Hello = (props) => (
+  <div>
+    <p>
+      Hello {props.name}, you are {props.age} years old
+    </p>
+  </div>
+);
+
+const Footer = () => (
+  <div>
+    greeting app created by <a href="https://github.com/mluukkai">mluukkai</a>
+  </div>
+);
 
 const App = () => {
-  const name = 'Peter'
-  const age = 10
+  const name = 'Peter';
+  const age = 10;
 
   return (
+    // <> and </> are React fragments, see: https://reactjs.org/docs/fragments.html
     <>
       <h1>Greetings</h1>
       <Hello name="Maya" age={26 + 10} />
       <Hello name={name} age={age} />
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
